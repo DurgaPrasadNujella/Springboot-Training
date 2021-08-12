dee071ffc14744e23c0e152df83c993cacae7468
DurgaPrasadNujella <durgaprasad.nujella@gmail.com>
2021-08-12 16:33:16 +0530

    Rest example

---
+++ basicexample/src/test/java/com/durga/basicexample/BasicexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durga.basicexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
