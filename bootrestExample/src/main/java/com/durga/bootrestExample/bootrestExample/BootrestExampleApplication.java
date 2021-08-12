b9ed92ca40b1da5a6f7eab18b4e7b6be523b0248
DurgaPrasadNujella <durgaprasad.nujella@gmail.com>
2021-08-12 17:52:35 +0530

    boot Rest example

---
+++ bootrestExample/src/main/java/com/durga/bootrestExample/bootrestExample/BootrestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.durga.bootrestExample.bootrestExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootrestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootrestExampleApplication.class, args);
+	}
+
+}
---
+++ bootrestExample/src/test/java/com/durga/bootrestExample/bootrestExample/BootrestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durga.bootrestExample.bootrestExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootrestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
