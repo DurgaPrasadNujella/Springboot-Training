a503deba2753ca2dc7d00c4fc6fefc7dc7214d47
DurgaPrasadNujella <durgaprasad.nujella@gmail.com>
2021-08-12 17:39:16 +0530

    Rest example1

---
+++ basicexample1/src/main/java/com/durga/basicexample/BasicexampleApplication.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicexampleApplication.class, args);
+	}
+
+}
